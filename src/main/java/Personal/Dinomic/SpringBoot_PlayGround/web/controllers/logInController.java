8ce7d7afd43ffde32596d2482ed4be9a1a5b0158
Dinomic <hthangnguyen1101@gmail.com>
2021-05-16 17:04:54 +0700

    spring boot security

--- src/main/java/Personal/Dinomic/SpringBoot_PlayGround/web/controllers/logInController.java
+++ src/main/java/Personal/Dinomic/SpringBoot_PlayGround/web/controllers/logInController.java
@@ -1,5 +1,13 @@
 package Personal.Dinomic.SpringBoot_PlayGround.web.controllers;
 
-public class logInController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class logInController {
+    @RequestMapping({"/", "/signIn"})
+    public String home(Model model){
+        return "signIn";
+    }
 }
