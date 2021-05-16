8f2c642628d5a566f3b0112da46189ba2d843535
Dinomic <hthangnguyen1101@gmail.com>
2021-05-16 16:50:46 +0700

    play with database

---
+++ src/main/java/Personal/Dinomic/SpringBoot_PlayGround/web/controllers/logInController.java
@@ -0,0 +1,5 @@
+package Personal.Dinomic.SpringBoot_PlayGround.web.controllers;
+
+public class logInController {
+
+}
---
+++ src/main/resources/db/migration/V0.0.0__Initialize_database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE account (
+    account_id INT NOT NULL,
+    account_name VARCHAR(50) NOT NULL,
+    account_password VARCHAR(50) NOT NULL
+);
