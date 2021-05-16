52aba4aff681e724194b1dc0f5d3717efaec6e17
Dinomic <hthangnguyen1101@gmail.com>
2021-05-16 11:08:27 +0700

    initialize spring boot project

---
+++ src/main/java/Personal/Dinomic/SpringBoot_PlayGround/SpringBootPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package Personal.Dinomic.SpringBoot_PlayGround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlayGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Personal/Dinomic/SpringBoot_PlayGround/SpringBootPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package Personal.Dinomic.SpringBoot_PlayGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
